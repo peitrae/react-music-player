fbd2193b7c5c82dd9608cb9ac96af45f0e44ef83
Peitra Erdi <peitrae@gmail.com>
2021-03-14 16:02:43 +0700

    Add logout button

--- src/views/Main/index.js
+++ src/views/Main/index.js
@@ -2,9 +2,14 @@ import React from "react";
 
 import Page from "../../components/layouts/Page";
 import Player from "../../components/modules/Player";
+import { logout } from "../../utils/inMemoryToken";
+import { LogoutButton } from "./styles";
 
 const Main = () => (
   <Page>
+    <LogoutButton variant="outlined" size="sm" color="white" onClick={logout}>
+      Logout
+    </LogoutButton>
     <Player />
   </Page>
 );
---
+++ src/views/Main/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import Button from "../../components/inputs/Button";
+
+export const LogoutButton = styled(Button)`
+  position: absolute;
+  top: 1.25rem;
+  right: 1.25rem;
+`;
