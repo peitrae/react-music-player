524e598ba99c54f2a0145b2f8222296dab9e3bf2
Peitra Erdi <peitrae@gmail.com>
2021-03-14 19:05:14 +0700

    Create react hook to connect player with spotify SDK

---
+++ src/hooks/useSpotify.js
@@ -0,0 +1,40 @@
+import { useEffect } from "react";
+
+const useSpotify = ({ token }) => {
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://sdk.scdn.co/spotify-player.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+    window.onSpotifyWebPlaybackSDKReady = () => {
+      const spotifySDK = new window.Spotify.Player({
+        name: "Songbird",
+        getOAuthToken: (cb) => {
+          cb(token);
+        },
+      });
+
+      spotifySDK.on("initialization_error", ({ message }) => {
+        console.error(`Initialization Error: ${message}`);
+      });
+
+      spotifySDK.on("authentication_error", ({ message }) => {
+        console.error(`Authentication Error: ${message}`);
+      });
+
+      spotifySDK.on("account_error", ({ message }) => {
+        console.error(`Account Error: ${message}`);
+      });
+
+      spotifySDK.on("playback_error", ({ message }) => {
+        console.error(`Playback Error: ${message}`);
+      });
+
+      spotifySDK.connect();
+    }; // eslint-disable-next-line
+  }, [token]);
+};
+
+export default useSpotify;
